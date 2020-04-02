23d2129ccc6bf6f03523b53adb92e842319eb008
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-01 21:43:58 -0400

    syntax fun

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const calculateSubTotals = (items) => items.map(updateTotal)
 
 const updateTotal = (item) => {
-  let returnItem = item
-
-  returnItem = { ...item, total: parseFloat((item.price * item.quantity * (1 - item.discount)).toFixed(2)) }
-
-  return returnItem
+  return { ...item, total: parseFloat((item.price * item.quantity * (1 - item.discount)).toFixed(2)) }
 }
 
 module.exports = calculateSubTotals
